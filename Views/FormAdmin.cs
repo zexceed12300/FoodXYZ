677b73432ce4d10023160816f09e2149a4db448e
r4venz <zexceed12300@gmail.com>
2023-10-11 01:17:31 +0700

    initial commit
    * Add FormAdmin form
    * Add FormKelolaUser childform
    * Add FormKelolaLaporan childform

---
+++ Views/FormAdmin.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FoodXYZ.Views
+{
+    public partial class FormAdmin : Form
+    {
+        public FormAdmin()
+        {
+            InitializeComponent();
+
+            this.panelContainer.Controls.Clear();
+            FormKelolaUser form = new FormKelolaUser()
+            {
+                Dock = DockStyle.Fill,
+                TopLevel = false,
+                TopMost = true
+            };
+            this.panelContainer.Controls.Add(form);
+            form.Show();
+        }
+
+        private void btnKelolaUser_Click(object sender, EventArgs e)
+        {
+            this.panelContainer.Controls.Clear();
+            FormKelolaUser form = new FormKelolaUser()
+            {
+                Dock = DockStyle.Fill,
+                TopLevel = false,
+                TopMost = true
+            };
+            this.panelContainer.Controls.Add(form);
+            form.Show();
+        }
+
+        private void btnKelolaLaporan_Click(object sender, EventArgs e)
+        {
+            this.panelContainer.Controls.Clear();
+            FormKelolaLaporan form = new FormKelolaLaporan()
+            {
+                Dock = DockStyle.Fill,
+                TopLevel = false,
+                TopMost = true,
+            };
+            this.panelContainer.Controls.Add(form);
+            form.Show();
+        }
+    }
+}
